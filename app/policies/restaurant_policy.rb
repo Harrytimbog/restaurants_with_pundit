92013847afe1630377d5791e120e472df426d782
Timilehin Emmanuel <Harriemannie@gmail.com>
2020-05-02 02:44:36 +0100

    Authorization and pundit

---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,32 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+      #scope.where(user: user)
+    end
+  end
+
+  def show?
+    true  # Anyone can view a restaurant
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    user_is_owner_or_admin?
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+
+  def destroy?
+    user_is_owner_or_admin?
+  end
+
+  private
+  def user_is_owner_or_admin?
+    record.user == user || user.admin
+  end
+
+end
